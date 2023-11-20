32e1d81fe89566063e38192f99b9c890f493f0bf
Kirellos Wasfy <94471342+kirellos-emad1@users.noreply.github.com>
2023-11-20 08:19:19 +0200

    Update builder.ts

--- graphql/builder.ts
+++ graphql/builder.ts
@@ -22,9 +22,32 @@ builder.queryType({
   }),
 });
 builder.inputType("InjuryInput", {
+builder.prismaObject("User", {
   fields: (t) => ({
-    area: t.string({ required: true }),
-    description: t.string({ required: true }),
+    id: t.exposeID("id"),
+    email: t.exposeString("email"),
+    injuryLists: t.relation("injuryLists"),
   }),
 });
+builder.queryField("getUserData", (t) =>
+  t.prismaField({
+    type: "User",
+    resolve: async (query, _parent, _args, ctx) => {
+      if (!(await ctx).user) {
+        throw new Error("You have to be logged in to perform this action");
+      }
+
+      const user = await prisma.user.findUnique({
+        ...query,
+        where: {
+          email: (await ctx).user?.email,
+        },
+      });
+
+      if (!user) throw Error("User does not exist");
+
+      return user;
+    },
+  })
+);
 builder.mutationType({});
